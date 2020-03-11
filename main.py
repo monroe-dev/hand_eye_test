d59156437570f1ab54296ad2fea7e1cb60f48599
monroe-dev <shin.hyunsoo.hy@gmail.com>
2020-03-11 12:08:56 +0100

    Second

--- main.py
+++ main.py
@@ -24,10 +24,13 @@ for i in range(numMethod):
     print('Position: ', mean_t_diff[i], '/', std_t_diff[i], '/', max_t_diff[i])
 
 # Graph
-plt1 = plt.subplot(221)
-plt2 = plt.subplot(222)
-plt3 = plt.subplot(223)
-plt4 = plt.subplot(224)
+fig = plt.figure(figsize=(12, 12))
+# ax = fig.add_subplot(111)
+
+plt1 = fig.add_subplot(221)
+plt2 = fig.add_subplot(222)
+plt3 = fig.add_subplot(223)
+plt4 = fig.add_subplot(224)
 
 for i in range(numMethod):
     plt1.plot(rvec_diff[:, i])
@@ -35,15 +38,21 @@ for i in range(numMethod):
     plt1.set_xlabel('Measurement number')
     plt2.set_xlabel('Measurement number')
 
+plt1.grid(True)
+plt2.grid(True)
+plt3.grid(True)
+plt4.grid(True)
 plt1.set_title('Orientation error')
 plt2.set_title('Position error')
 plt1.legend(('TSA', 'PAR', 'HOR', 'DAN', 'AND'))
 plt2.legend(('TSA', 'PAR', 'HOR', 'DAN', 'AND'))
 
 plt3.boxplot(rvec_diff)
-plt3.set_xlabel('Method')
 plt4.boxplot(t_diff)
+plt3.set_xlabel('Method')
 plt4.set_xlabel('Method')
 
 # plt.grid(True)
+
+fig.savefig('result_handeye_calibration.png', dpi=300)
 plt.show()

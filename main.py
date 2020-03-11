d50f1743d2c013bbc8927946015a7e47d38ec1b0
monroe-dev <shin.hyunsoo.hy@gmail.com>
2020-03-11 11:56:22 +0100

    Second

--- main.py
+++ main.py
@@ -1,19 +1,49 @@
+# import matplotlib as mpl
+# mpl.use('agg')
+
 import matplotlib.pyplot as plt
 import simulate
 
-numTest = 2
+numTest = 10
 numPose = 10
-rvec_diff, t_diff = simulate.validate(numTest, numPose)
+numMethod = 4
+# CALIB_HAND_EYE_DANIILIDIS = 4
+# CALIB_HAND_EYE_ANDREFF = 3
+# CALIB_HAND_EYE_HORAUD = 2
+# CALIB_HAND_EYE_PARK = 1
+# CALIB_HAND_EYE_TSAI = 0
 
-print(rvec_diff.shape)
+# Test
+rvec_diff, t_diff, mean_rvec_diff, mean_t_diff, std_rvec_diff, std_t_diff, max_rvec_diff, max_t_diff = simulate.validate(numTest, numPose, numMethod)
+
+# Print
+print('Error analyis')
+for i in range(numMethod):
+    print('Method', i, ': (Mean / Std / Max)')
+    print('Orientation(Rotation vector): ', mean_rvec_diff[i], '/', std_rvec_diff[i], '/', max_rvec_diff[i])
+    print('Position: ', mean_t_diff[i], '/', std_t_diff[i], '/', max_t_diff[i])
+
 # Graph
-# plt.subplot(211)
-# plt.plot(rvec_diff)
-# plt.ylabel('Orientation error')
+plt1 = plt.subplot(221)
+plt2 = plt.subplot(222)
+plt3 = plt.subplot(223)
+plt4 = plt.subplot(224)
+
+for i in range(numMethod):
+    plt1.plot(rvec_diff[:, i])
+    plt2.plot(t_diff[:, i])
+    plt1.set_xlabel('Measurement number')
+    plt2.set_xlabel('Measurement number')
+
+plt1.set_title('Orientation error')
+plt2.set_title('Position error')
+plt1.legend(('TSA', 'PAR', 'HOR', 'DAN', 'AND'))
+plt2.legend(('TSA', 'PAR', 'HOR', 'DAN', 'AND'))
+
+plt3.boxplot(rvec_diff)
+plt3.set_xlabel('Method')
+plt4.boxplot(t_diff)
+plt4.set_xlabel('Method')
+
 # plt.grid(True)
-#
-# plt.subplot(212)
-# plt.plot(t_diff)
-# plt.ylabel('Position error')
-# plt.grid(True)
-# plt.show()
+plt.show()
--- simulate.py
+++ simulate.py
@@ -21,6 +21,9 @@ def generateNewPose(nPose, noise):
     """ T_hand2eye, T_base2world are constant(fixed) matrix.
         T_base2hand, T_eye2world are different matrix at the each pose."""
 
+    eye_noise = [-3, 3, -3, 3]      # [min_rvec, max_rvec, min_t, max_t]
+    robot_noise = [-3, 3, -3, 3]      # [min_rvec, max_rvec, min_t, max_t]
+    # Generate
     R_hand2eye, t_hand2eye = newPose(np.deg2rad(10), np.deg2rad(50), 0.05, 0.5)
     T_hand2eye = homogeneous_matrix(R_hand2eye, t_hand2eye)
     R_base2world, t_base2world = newPose(np.deg2rad(5), np.deg2rad(85), 0.5, 3.5)
@@ -44,32 +47,31 @@ def generateNewPose(nPose, noise):
         if noise:
             """ Estimation of world coordinate using camera(eye) has some error,
                 and the robot also usually exist positioning error."""
-            # Estimation of world
+            # World coordinate
             # Add to arbitrary noise.
-            # variable_ means each element.
             R_eye2world_noise = T_eye2world[:3, :3]
             rvec_eye2world_noise = np.empty((3, 1))
             # Orientation
             cv2.Rodrigues(R_eye2world_noise, rvec_eye2world_noise)
-            rvec_eye2world_noise = np.add(rvec_eye2world_noise, np.random.uniform(-1, 1, (3, 1)))
+            rvec_eye2world_noise = np.add(rvec_eye2world_noise, np.random.uniform(eye_noise[0], eye_noise[1], (3, 1)))
             cv2.Rodrigues(rvec_eye2world_noise, R_eye2world_noise)
             R_eye2world_ = R_eye2world_noise
             # Position
             t_eye2world_noise = T_eye2world[:3, 3]
-            t_eye2world_noise = np.add(np.expand_dims(t_eye2world_noise, axis=1), np.random.uniform(-1, 1, (3, 1)))
+            t_eye2world_noise = np.add(np.expand_dims(t_eye2world_noise, axis=1), np.random.uniform(eye_noise[2], [3], (3, 1)))
             t_eye2world_ = t_eye2world_noise
 
-            # Estimation of robot hand
+            # Robot hand
             # Orientation
             R_base2hand_noise = T_base2hand[:3, :3]
             rvec_base2hand_noise = np.empty((3, 1))
             cv2.Rodrigues(R_base2hand_noise, rvec_base2hand_noise)
-            rvec_base2hand_noise = np.add(rvec_base2hand_noise, np.random.uniform(-1, 1, (3, 1)))
+            rvec_base2hand_noise = np.add(rvec_base2hand_noise, np.random.uniform(robot_noise[0], robot_noise[1], (3, 1)))
             cv2.Rodrigues(rvec_base2hand_noise, R_base2hand_noise)
             R_base2hand_ = R_base2hand_noise
             # Position
             t_base2hand_noise = T_base2hand[:3, 3]
-            t_base2hand_noise = np.add(np.expand_dims(t_base2hand_noise, axis=1), np.random.uniform(-1, 1, (3, 1)))
+            t_base2hand_noise = np.add(np.expand_dims(t_base2hand_noise, axis=1), np.random.uniform(robot_noise[2], robot_noise[3], (3, 1)))
             t_base2hand_ = t_base2hand_noise
 
         # Rotation matrix and translation vector at the every pose
@@ -77,28 +79,29 @@ def generateNewPose(nPose, noise):
         t_eye2world = np.concatenate((t_eye2world, np.expand_dims(t_eye2world_, axis=0)), axis=0)
         R_base2hand = np.concatenate((R_base2hand, np.expand_dims(R_base2hand_, axis=0)), axis=0)
         t_base2hand = np.concatenate((t_base2hand, np.expand_dims(t_base2hand_, axis=0)), axis=0)
+
     # Because first array was zeros..
     return R_eye2world[1:], t_eye2world[1:], R_base2hand[1:], t_base2hand[1:], R_hand2eye, t_hand2eye
 
 
-def validate(numTest, numPose):
+def validate(numTest, numPose, numMethod):
     noise = True
     # Allocation
     rvec_hand2eye_true = np.empty((3, 1))
     R_hand2eye_est = np.empty((3, 3))
     t_hand2eye_est = np.empty((3, 1))
     rvec_hand2eye_est = np.empty((3, 1))
-    rvec_diff_ = np.empty(1)
-    t_diff_ = np.empty(1)
     # Five methods
-    rvec_diff = np.empty((1, 5))
-    t_diff = np.empty((1, 5))
+    rvec_diff = np.empty((1, numMethod))
+    t_diff = np.empty((1, numMethod))
 
     for i in range(numTest):
         # Generate a number of poses
         R_eye2world, t_eye2world, R_base2hand, t_base2hand, R_hand2eye_true, t_hand2eye_true = generateNewPose(numPose, noise)
+        rvec_diff_ = np.empty(1)
+        t_diff_ = np.empty(1)
 
-        for j in range(5):
+        for j in range(numMethod):
             # Estimation of transformation from robot hand to camera
             cv2.calibrateHandEye(R_base2hand, t_base2hand, R_eye2world, t_eye2world, R_hand2eye_est, t_hand2eye_est, method=j)
             # Validate
@@ -108,17 +111,24 @@ def validate(numTest, numPose):
             rvec_diff__ = np.linalg.norm(np.subtract(rvec_hand2eye_true, rvec_hand2eye_est))
             # Translation
             t_diff__ = np.linalg.norm(np.subtract(t_hand2eye_true, t_hand2eye_est))
-
+            # The result vectors at the each method
             rvec_diff_ = np.append(rvec_diff_, np.expand_dims(rvec_diff__, axis=0), axis=0)
             t_diff_ = np.append(t_diff_, np.expand_dims(t_diff__, axis=0), axis=0)
 
-        rvec_diff_ = rvec_diff_.reshape(1, 6)
-        t_diff_ = t_diff_.reshape(1, 6)
-        rvec_diff = np.concatenate((rvec_diff, rvec_diff_[:, 1:6]), axis=0)
-        t_diff = np.concatenate((t_diff, t_diff_[:, 1:6]), axis=0)
+        rvec_diff_ = rvec_diff_.reshape(1, numMethod + 1)
+        t_diff_ = t_diff_.reshape(1, numMethod + 1)
+        # The result vectors at the each test
+        rvec_diff = np.concatenate((rvec_diff, rvec_diff_[:, 1:numMethod + 1]), axis=0)
+        t_diff = np.concatenate((t_diff, t_diff_[:, 1:numMethod + 1]), axis=0)
 
-    # print(rvec_diff[])
-    # print(t_diff.shape)
+    mean_rvec_diff = np.mean(rvec_diff[1:, :], axis=0)
+    mean_t_diff = np.mean(t_diff[1:, :], axis=0)
 
-    return rvec_diff[1:], t_diff[1:]
+    std_rvec_diff = np.std(rvec_diff[1:, :], axis=0)
+    std_t_diff = np.std(t_diff[1:, :], axis=0)
+
+    max_rvec_diff = np.max(rvec_diff[1:, :], axis=0)
+    max_t_diff = np.max(t_diff[1:, :], axis=0)
+
+    return rvec_diff[1:], t_diff[1:], mean_rvec_diff, mean_t_diff, std_rvec_diff, std_t_diff, max_rvec_diff, max_t_diff
 
